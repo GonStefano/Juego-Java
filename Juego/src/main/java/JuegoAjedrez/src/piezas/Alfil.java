cefd3738b6fb71e7f9afcf69684a53a763a6f9f2
Stefano Gonzalez <stefano.gonzalez@educa.madrid.org>
2025-05-20 20:16:10 +0200

    Add movimiento de caballo y alfil

--- Juego/src/main/java/JuegoAjedrez/src/piezas/Alfil.java
+++ Juego/src/main/java/JuegoAjedrez/src/piezas/Alfil.java
@@ -14,4 +14,25 @@ public class Alfil extends Pieza{
             return Colores.AMARILLO_BRIGHT + "B";
         }
     }
+
+    public boolean movimiento(Posicion destino) {
+        Posicion origen = this.getPosicion();
+        int diagonalX = Math.abs(destino.getX() - origen.getX());
+        int diagonalY = Math.abs(destino.getY() - origen.getY());
+
+        // (=============== || =============================== && ===============================)
+        if (destino == null || origen.getX() == destino.getX() && origen.getY() == destino.getY()) {
+            System.out.println("Movimiento invalido del alfil");
+            return false;
+        }
+
+        // (======================)
+        if (diagonalX == diagonalY) { // al igualar los valores absolutos de x e y determinamos que al ser los mismos valores estan en la diagonal, cosa que por ejemplo con el caballo va a ser molesta de gestionar
+            System.out.println("Movimiento diagonal del alfil valido");
+            return true;
+        }
+
+        System.out.println("Movimiento invalido del alfil");
+        return false;
+    }
 }
--- Juego/src/main/java/JuegoAjedrez/src/piezas/Caballo.java
+++ Juego/src/main/java/JuegoAjedrez/src/piezas/Caballo.java
@@ -14,4 +14,24 @@ public class Caballo extends Pieza {
             return Colores.AMARILLO_BRIGHT + "C";
         }
     }
+
+    public boolean movimiento(Posicion destino) {
+        Posicion origen = this.getPosicion();
+
+        if (destino == null || (origen.getX() == destino.getX() && origen.getY() == destino.getY())) {
+            System.out.println("Movimiento inválido del caballo");
+            return false;
+        }
+
+        int dx = Math.abs(origen.getX() - destino.getX());
+        int dy = Math.abs(origen.getY() - destino.getY());
+
+        // movimiento en forma de L 2x1 o 1x2
+        if ((dx == 2 && dy == 1) || (dx == 1 && dy == 2)) {
+            return true;
+        }
+
+        System.out.println("Movimiento inválido del caballo");
+        return false;
+    }
 }
